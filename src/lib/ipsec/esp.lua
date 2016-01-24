0bd2fbface9a66e43f371c69eeafcbcf6ce4b648
hb9cwp <rolf.sommerhalder@alumni.ethz.ch>
2016-01-24 11:55:39 +0100

    better be safe than sorry: buffer with blocksize although last Byte unused, used it for canary?

--- src/lib/ipsec/esp.lua
+++ src/lib/ipsec/esp.lua
@@ -23,7 +23,7 @@ local esp_v6_encrypt = {}
 
 function esp_v6_encrypt:new (conf)
    local o = esp_v6_new(conf)
-   o.pad_buf = ffi.new("uint8_t[?]", o.aes_128_gcm.blocksize-1)
+   o.pad_buf = ffi.new("uint8_t[?]", o.aes_128_gcm.blocksize)
    o.esp_buf = ffi.new("uint8_t[?]", o.aes_128_gcm.aad_size)
    -- Fix me https://tools.ietf.org/html/rfc4303#section-3.3.3
    o.esp = esp:new_from_mem(o.esp_buf, esp_length)
@@ -144,7 +144,7 @@ function selftest ()
 [[abcdefghijklmnopqrstuvwxyz
 ABCDEFGHIJKLMNOPQRSTUVWXYZ
 0123456789012345678901234567890123456789012345678901234567890123456789012]]
-   )
+)
 
 --[[
 for i=0,258 do
